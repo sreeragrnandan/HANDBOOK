60b6f7c001b1738ec9ccc291fb3a2e02c7a57cb2
sreerag <sreeragraghunandan@gmail.com>
2019-02-07 00:39:16 +0530

    DATA BASE CONNECTION ESTABLISHED

--- handbook/calender/models.py
+++ handbook/calender/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# Create your models here.V
+class Contacts(models.Model):
+    index = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50 ,unique=False)
+    url = models.URLField(unique=True)
+    number = models.CharField(max_length=50 ,unique=True)
+    department = models.CharField(max_length=50 ,unique=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
