0583c663f1daa6ad8ad9624d65c723aaa99f6973
sreerag <sreeragraghunandan@gmail.com>
2019-02-14 22:17:29 +0530

    database sql linked

--- handbook/handbook/settings.py
+++ handbook/handbook/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,7 +80,7 @@ WSGI_APPLICATION = 'handbook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'localhost',
+        'NAME': 'handbook',
         'USER':'root',
         'PASSWORD':'',
         'HOST':'127.0.0.1',
