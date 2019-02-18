01f6ffba3848e8b48d5ab0f72fc3489614d7139d
sreerag <sreeragraghunandan@gmail.com>
2019-02-19 00:55:22 +0530

    calendrModified

--- handbook/calender/views.py
+++ handbook/calender/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render,loader
 from django.http import HttpResponse
 from django.db import connection
 def index(request):
+    
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM calender calender WHERE id = 1")
         row = cursor.fetchone()
-    return HttpResponse(row)
+    template = loader.get_template('calender/calender.html')
+    return HttpResponse(template.render(row, request))
--- handbook/contacts/url.py
+++ handbook/contacts/url.py
@@ -1,3 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from django.urls import path
 from . import views
 app_name = "contacts"
