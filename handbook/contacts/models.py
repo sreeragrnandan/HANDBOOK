ab849270ffebca7e17be945507c72081cd684b56
sreerag <sreeragraghunandan@gmail.com>
2019-03-01 23:54:39 +0530

    contact updated

--- handbook/contacts/models.py
+++ handbook/contacts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class contact(models.Model):
-    name = models.CharField(max_length=80)
-    phone = models.CharField(max_length=20,unique=True)
-    email = models.EmailField()
+    Name = models.CharField(max_length=80)
+    Designation = models.CharField(max_length=100)
+    Qualification = models.CharField(max_length=80)
+    Email = models.EmailField()
+    Phone_Number = models.CharField(max_length=20,unique=True)
+    Photos = models.CharField(max_length = 100, default='0000000')
