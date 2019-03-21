91abf74b08f66c26818a08e57383fc2106eb54f2
sreerag <sreeragraghunandan@gmail.com>
2019-03-21 16:55:47 +0530

    basic design

--- handbook/contacts/models.py
+++ handbook/contacts/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class contact(models.Model):
+    Department = models.CharField(max_length = 80,default='NULL')
     Name = models.CharField(max_length=80)
     Designation = models.CharField(max_length=100)
     Qualification = models.CharField(max_length=80)
     Email = models.EmailField()
     Phone_Number = models.CharField(max_length=20,unique=True)
-    Photos = models.CharField(max_length = 100, default='0000000')
+    StaffID = models.CharField(max_length = 100, default='JEC')
