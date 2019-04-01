91591838d747f031b7814b655e766ea4e9a97a29
sreerag <sreeragraghunandan@gmail.com>
2019-04-02 01:25:41 +0530

    django admin edited

--- handbook/contacts/models.py
+++ handbook/contacts/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 class contact(models.Model):
-    Department = models.CharField(max_length = 80,default='NULL')
+    DEPARTMENT_CHOICES = (
+        ('CSE','CSE'),('ECE','ECE'),('Administration','Administration'),('CIVIL','CIVIL')
+        ,('EEE','EEE'),('ME','ME'),('BSH','BSH'),('Office','Office'),('Library','Library'),
+        ('COMPUTER CENTER','COMPUTER CENTER'),('HOSTEL','HOSTEL'),
+    )
+    Department = models.CharField(max_length = 80,choices = DEPARTMENT_CHOICES , default='Select department' )
     Name = models.CharField(max_length=80)
-    Designation = models.CharField(max_length=100)
+    Designation_CHOICES = (
+        ('Assistant Professor','Assistant Professor'),('Associate Professor','Associate Professor'),('Bus Codinator','Bus Codinator')
+        ,('Professor','Professor'),('Trade Instructor','Trade Instructor')
+        ,('Office Assistant','Office Assistant'),('Accountant','Accountant'),('Office Manager','Office Manager'),('Principal','Principal')
+        ,('Secretary to Principal','Secretary to Principal'),('Driver','Driver'),('Manager','Manager'),
+        ('Campus Head','Campus Head'),('Asst. Manager','Asst. Manager'),('Administrator','Administrator'),
+        ('System Administrator','System Administrator'),('Manager','Manager'),('Chairman','Chairman'),('Hostel Warden','Hostel Warden'),
+        ('',''),
+    )
+
+    Designation = MultiSelectField(choices = Designation_CHOICES)
     Qualification = models.CharField(max_length=80)
     Email = models.EmailField()
     Phone_Number = models.CharField(max_length=20,unique=True)
