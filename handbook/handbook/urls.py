174736f50450885630b06628ae60a6ad6f7c6033
sreerag <sreeragraghunandan@gmail.com>
2019-04-24 10:23:29 +0530

    submiton commit

--- handbook/handbook/urls.py
+++ handbook/handbook/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('calender.url')),
     path('admin/', admin.site.urls),
     path('index/', include('index.url')),
     path('contacts/', include('contacts.url')),
